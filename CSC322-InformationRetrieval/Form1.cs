9f73acd0286ac5b6f66b765462dbada09138f40c
Feyisayo Sonubi <feyisayo.sonubi@outlook.com>
2016-09-21 13:48:26 +0100

    Initial commit

---
+++ CSC322-InformationRetrieval/Form1.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace CSC322_InformationRetrieval
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
