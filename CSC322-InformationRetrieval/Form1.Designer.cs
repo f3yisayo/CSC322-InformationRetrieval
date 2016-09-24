1e0a8cc9d1fa2346ace6fa3eff5238d6e8e14d8e
Feyisayo Sonubi <feyisayo.sonubi@outlook.com>
2016-09-24 01:25:49 +0100

    Add Folder Browser Dialog to allow the user choose a directory to search for files.

--- CSC322-InformationRetrieval/Form1.Designer.cs
+++ CSC322-InformationRetrieval/Form1.Designer.cs
@@ -30,12 +30,16 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pathTextbox = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.browseButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(133, 47);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(144, 131);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
@@ -44,8 +48,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(692, 458);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(414, 584);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(133, 41);
             this.button1.TabIndex = 1;
@@ -53,11 +58,48 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(144, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Directory:";
+            // 
+            // pathTextbox
+            // 
+            this.pathTextbox.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pathTextbox.Location = new System.Drawing.Point(278, 46);
+            this.pathTextbox.Name = "pathTextbox";
+            this.pathTextbox.Size = new System.Drawing.Size(303, 30);
+            this.pathTextbox.TabIndex = 3;
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // browseButton
+            // 
+            this.browseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.browseButton.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.browseButton.Location = new System.Drawing.Point(610, 44);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(107, 33);
+            this.browseButton.TabIndex = 4;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(945, 513);
+            this.ClientSize = new System.Drawing.Size(945, 648);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.pathTextbox);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
@@ -72,6 +114,10 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pathTextbox;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button browseButton;
     }
 }
 
--- CSC322-InformationRetrieval/Form1.cs
+++ CSC322-InformationRetrieval/Form1.cs
@@ -1,23 +1,35 @@
-﻿using System.Collections.Generic;
-using System.Text;
+﻿using System.IO;
 using System.Windows.Forms;
-using Toxy;
 
 namespace CSC322_InformationRetrieval
 {
     public partial class Form1 : Form
     {
+        DialogResult dialogResult;
+        string folderPath;
+
         public Form1()
         {
-
             InitializeComponent();
         }
 
         private void button1_Click(object sender, System.EventArgs e)
         {
-            //display indexing results
-            textBox1.Text = new Indexer().Index(new System.IO.DirectoryInfo(@"C:\Users\Feyisayo\Downloads"));
+            // Clear the textbox before displaying anything new
+            textBox1.Clear();
+            // Display indexed results
+            textBox1.Text = new Indexer().Index(new DirectoryInfo(folderPath));
         }
 
+        private void browseButton_Click(object sender, System.EventArgs e)
+        {
+            dialogResult = folderBrowserDialog.ShowDialog();
+
+            if (dialogResult == DialogResult.OK)
+            {
+                folderPath = folderBrowserDialog.SelectedPath;
+                pathTextbox.Text = folderPath;
+            }
+        }
     }
 }
